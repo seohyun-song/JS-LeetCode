9115d6d8a8145c766a4263e12a0e5cd7c1c8322c
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2020-10-11 20:11:44 +0900

    commit

--- easy/string/maxDepth.js
+++ easy/string/maxDepth.js
@@ -8,7 +8,7 @@
         ● depth('') = 0
         ● depth(A+B) = max(depth(A), depth(B)), where A and B are VPS's.
         ● depth('('+A+')') = 1+depth(A), where A is a VPS.
-    Given a VPS represented as string s, return the nesting depth of s.
+    ** Given a VPS represented as string s, return the nesting depth of s.
 
     Constrainis:
         ● 1 <= s.length <= 100
@@ -16,25 +16,44 @@
         ● It is guaranteed that parentheses expression s is a VPS
  */
 var maxDepth = function(s) {
-    var max = 0;
-    var totalMax = 0;
-    var stack = [];
+    if(s.length == 0) return 0;
+    var answer = 0;
+    var count = 0;
     
+    //The depth of any character in the VPS is the (number of left brackets before it) - (number of right brackets before it).
     for(var i=0; i<s.length; i++){
-        if(s[i] == '('){
-            max++;
-            stack.push('(');
-            
-            if(max>totalMax) totalMax=max;
-            
+        if(s[i] == '(') {
+            count++;
         } else if(s[i] == ')'){
-            var close = stack.pop();
-            if(max>0 && close == '('){
-                max--;
-            }else{return -1;}
+            count--;
+            //s[i]가 숫자가 들어오면 answer값에 최대 depth 값을 재할당함
+        } else if(!isNaN(s[i])) {
+            answer = Math.max(answer, count);
         }
     }
-    if(stack.length != 0) return -1;
+    return answer;
     
-    return totalMax;
+/* stack
+        var max = 0;
+        var totalMax = 0;
+        var stack = [];
+        
+        for(var i=0; i<s.length; i++){
+            if(s[i] == '('){
+                max++;
+                stack.push('(');
+                
+                if(max>totalMax) totalMax=max;
+
+            } else if(s[i] == ')'){
+                var close = stack.pop();
+                if(max>0 && close == '('){
+                    max--;
+                }else {return -1;}
+            }
+        }
+        if(stack.length != 0) return -1;
+        
+        return totalMax;
+*/
 };
