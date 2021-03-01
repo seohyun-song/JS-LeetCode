33c9fbb66ec86bce09e8427fe825f7587818fd7d
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2021-03-01 21:31:25 +0900

    commit

---
+++ easy/array/countItemsMatchingaRule.js
@@ -0,0 +1,32 @@
+/*
+    You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+    The ith item is said to match the rule if one of the following is true:
+
+    ruleKey == "type" and ruleValue == typei.
+    ruleKey == "color" and ruleValue == colori.
+    ruleKey == "name" and ruleValue == namei.
+
+    Return the number of items that match the given rule.
+
+    Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+    Output: 1
+    Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    var result = 0;
+    if(ruleKey == 'type'){
+        for(var i = 0; i < items.length; i++){
+            if(ruleValue == items[i][0]) result++;
+        }
+    } else if(ruleKey == 'color'){
+        for(var i = 0; i < items.length; i++){
+            if(ruleValue == items[i][1]) result++;
+        }
+    } else if(ruleKey == 'name'){
+        for(var i = 0; i < items.length; i++){
+            if(ruleValue == items[i][2]) result++;
+        }
+    }
+  
+    return result;
+};
