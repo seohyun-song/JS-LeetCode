52c8ace6086dfa91f94d8bca9deb695c8c0ebd6a
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2021-03-14 13:15:37 +0900

    Create FlippingAnImage.js

---
+++ easy/array/FlippingAnImage.js
@@ -0,0 +1,28 @@
+/*
+    Flipping an Image
+    Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+    To flip an image horizontally means that each row of the image is reversed.
+    
+    For example, flipping [1,1,0] horizontally results in [0,1,1].
+    To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+    For example, inverting [0,1,1] results in [1,0,0].
+
+    * reverse(): 호출한 배열을 반전하고 원본 배열을 변형하며 그 참조를 반환함
+ */
+var flipAndInvertImage = function(image) {
+    var len = image[0].length;
+    for(var i = 0; i < image.length; i++) {
+        image[i].reverse();
+        
+        for(var j = 0; j < len; j++) {
+            if(image[i][j] == 1){
+                image[i][j] = 0;
+            } else {
+                image[i][j] = 1;
+            }
+        }
+    }
+    return image;
+    
+};
