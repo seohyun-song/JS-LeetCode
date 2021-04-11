64d08ad382462f6c5013952b0a3f27a77494264d
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2021-04-11 16:15:21 +0900

    Create truncateSentence.js

---
+++ easy/string/truncateSentence.js
@@ -0,0 +1,29 @@
+/*
+    Truncate Sentence
+    A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+    For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+    You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+    Example 1:
+        Input: s = "Hello how are you Contestant", k = 4
+        Output: "Hello how are you"
+        Explanation:
+        The words in s are ["Hello", "how" "are", "you", "Contestant"].
+        The first 4 words are ["Hello", "how", "are", "you"].
+        Hence, you should return "Hello how are you".
+    
+    Example 2:
+        Input: s = "chopper is not a tanuki", k = 5
+        Output: "chopper is not a tanuki"
+
+    Constraints:
+    k is in the range [1, the number of words in s].
+        1 <= s.length <= 500
+        s consist of only lowercase and uppercase English letters and spaces.
+        The words in s are separated by a single space.
+        There are no leading or trailing spaces.
+*/
+
+var truncateSentence = function(s, k) {
+    return s.split(' ').slice(0, k).join(' ');
+};
