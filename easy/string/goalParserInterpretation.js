0314b595549519468c7dfe9db7da3a6d4e90c2c5
seohyun-song <63182157+seohyun-song@users.noreply.github.com>
2021-04-17 12:23:21 +0900

    commit

--- easy/string/goalParserInterpretation.js
+++ easy/string/goalParserInterpretation.js
@@ -1,7 +1,6 @@
 /*
     Goal Parser Interpretation
-     * 스택을 활용한 문자열 문제
-    
+
     You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
     Given the string command, return the Goal Parser's interpretation of command.
 
@@ -18,20 +17,20 @@
     Input: command = "G()()()()(al)"
     Output: "Gooooal"
 
+    string은 유사배열 객체로서 array length속성을 사용가능하여 인덱스로 문자에 접근이 가능합니다.
    
  */
-
+// 스택을 활용한 풀이
 var interpret = function(command) {
-    let cmdArr = command.split('');
     let result = '';
     let stack = [];
     
-    for(var i = 0; i < cmdArr.length; i++) {
-        if(cmdArr[i] == 'G') {
+    for(var i = 0; i < command.length; i++) {
+        if(command[i] == 'G') {
             result += 'G';
-        } else if(cmdArr[i] == '(') {
+        } else if(command[i] == '(') {
             stack.push('(');
-        } else if( cmdArr[i] == ')') {
+        } else if( command[i] == ')') {
             stack.pop();
             if(stack.length == 0) {
                 result += 'o';
@@ -41,9 +40,18 @@ var interpret = function(command) {
                 stack = [];
             }
         } else {
-            stack.push(cmdArr[i]);
+            stack.push(command[i]);
         }
     }
     return result;
     
 };
+
+// 다른 사람 풀이 
+var interpret = function(command) {
+    let str = '';
+    let i = 0;
+    while( i < command.length) {
+
+    }
+}
