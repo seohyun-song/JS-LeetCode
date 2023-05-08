762a80f3e4329b8a55ab44f3f59b9adbab411e3b
seohyun-song <amanda94@naver.com>
2023-05-08 10:28:58 +0900

    230508/ [recap] two pointer

--- easy/array/removeDuplicatesFromSortedArray.js
+++ easy/array/removeDuplicatesFromSortedArray.js
@@ -3,10 +3,24 @@
     Do not aollocate extra space for another array, you must do this by modifying
     the input array in-place width O(1) extra memory
 */
-var removeDuplicates = function(nums) {
-    for(var i=nums.length-1; i>=0; i--){
-        if(nums[i-1] == nums[i]) nums.splice(i,1);
+var removeDuplicates = function (nums) {
+  for (var i = nums.length - 1; i >= 0; i--) {
+    if (nums[i - 1] == nums[i]) nums.splice(i, 1);
+  }
+  return nums.length;
+};
+
+// two pointer
+var removeDuplicates = function (nums) {
+  // slow runner
+  let i = 0;
+
+  // fast runner
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
     }
-    return nums.length;
-    
+  }
+  return i + 1;
 };
