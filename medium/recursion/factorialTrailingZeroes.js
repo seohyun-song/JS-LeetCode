c5010b5ee0c6d36432e891e0377aa65e265df3a2
seohyun song <seohyun@seohyunui-MacBookPro.local>
2023-06-14 19:52:35 +0900

    fix : refactoring

--- medium/recursion/factorialTrailingZeroes.js
+++ medium/recursion/factorialTrailingZeroes.js
@@ -15,31 +15,30 @@
   * 자바스크립트의 MAX_SAFE_INTEGER 값은 2^53 -1 으로 훨씬낮음(JavaScript Number -> 64비트 부동소수점)
   * 길이의 제약없이 정수를 다룰 수 있게 해주는 숫자형ㅇ니 BigInt 사용
   * BigInt는 BigInt형 숫자와 일반 Number를 섞어서 사용하지 못함
+  * 
+  * Runtime : 3652 ms/ Memory: 82.3 MB -> 리팩토링 필요
   */
 /**
  * @param {number} n
  * @return {number}
  */
 var trailingZeroes = function (n) {
-    const factorial = getFactorial(BigInt(n)).toString();
-    let i = factorial.length;
-    if (factorial[i - 1] === "0") {
-        let count = 1;
-        while (i >= 0) {
-            if (factorial[i - 1] === factorial[i - 2]) {
-                count++;
-                i--;
-            } else {
-                return count;
-            }
-        }
-    } else {
-        return 0;
-    }
+	const factorial = getFactorial(BigInt(n)).toString();
+	let i = factorial.length;
+	if (factorial[i - 1] !== "0") return 0;
+	let count = 1;
+	while (i >= 0) {
+		if (factorial[i - 1] === factorial[i - 2]) {
+			count++;
+			i--;
+		} else {
+			return count;
+		}
+	}
 };
 
 // recursive function & BigInt
 function getFactorial(n) {
-    if (n <= 1) return 1n;
-    return n * getFactorial(n - 1n);
+	if (n <= 1) return 1n;
+	return n * getFactorial(n - 1n);
 }
